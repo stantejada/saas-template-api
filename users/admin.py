295b0dfdc1f68b6c9eeb0d5675655da741945245
Stanley <stanleytejadaj@icloud.com>
2025-12-02 20:32:36 -0800

    Added admin and create superuser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'name', 'is_active', 'is_staff')
+    search_fields = ('email', 'name')
